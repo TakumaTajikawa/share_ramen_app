0044ee653cfb770eb6e98410c9b6ea38386c402b
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-08-30 01:05:28 +0900

    deviseコントローラの作成、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#top'
   get :about, to: 'home#about'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+  }
 end
