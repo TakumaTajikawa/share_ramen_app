ff45dbac072c0fb6e8fd72fb6d5f2db4eb6a2f5a
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-02 08:19:16 +0900

    users controllerの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
   def show
-    @user = User
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
   end
 end
