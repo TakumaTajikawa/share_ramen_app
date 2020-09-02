fa1fbf7cdd18d43f5be4f18ea54df7f656eabcbd
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-02 13:38:56 +0900

    プロフィール編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,18 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to user_path
+    if @user.id == current_user.id
+      @user.update(user_params)
+      flash[:success] = "プロフィールが更新されました！"
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email)
+  end 
 end
