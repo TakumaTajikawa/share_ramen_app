a3c2e93663294fbd05a851bb9ee6e7a0974e0357
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-03 14:12:43 +0900

    名前、email、自己紹介の文字数制限を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
-  validates :introduction, presence: false, length: { maximum: 400 } 
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 50 }
+  validates :introduction, presence: false, length: { maximum: 200 } 
   mount_uploader :image, ImageUploader
 end
