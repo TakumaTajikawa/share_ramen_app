6c7c37aaaf2595b4af7d1c186719d1a588b3357a
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-04 17:37:15 +0900

    ルーティングにpostsに関する記述を追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/create'
-  get 'posts/show'
-  get 'posts/edit'
-  root 'home#top'
+  root 'posts/index'
   get :about, to: 'home#about'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
   resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :posts
   
 end
