4bcbf87d5e477aa994ffb8ac671b15710604fba6
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-16 12:21:56 +0900

    commentsコントーラ、postsコントローラの変更、投稿詳細ページにコメント機能を仮作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comment.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:success] = "コメントしました"
+      redirect_back(fallback_location: post_path(@post.id))
+    else
+      flash[:danger] = "コメントできませんでした"
+      redirect_back(fallback_location: post_path(@post.id))
+    end 
+  end 
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comment.find(params[:id])
+    @comment.destroy
+    flash[:success] = "コメントを削除しました"
+    redirect_back(fallback_location: post_path(@post.id))
+  end 
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+end
