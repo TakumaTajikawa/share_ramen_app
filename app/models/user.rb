afcee57c765a811aa0f87337b925f067ff3ff4b7
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-17 12:47:20 +0900

    userモデルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comment, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
@@ -9,6 +13,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :introduction, presence: false, length: { maximum: 200 } 
   mount_uploader :image, ImageUploader
+  
+
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
 
   def posts
     return Post.where(user_id: self.id)
