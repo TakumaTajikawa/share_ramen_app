73c12b15a70d4474349be92670c7b4e52a694433
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-17 18:34:53 +0900

    relationshipsコントローラの変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,12 @@
 class RelationshipsController < ApplicationController
+  
   def create
-    @user = User.find(params[:user_id])
-    follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    follow.save
-    redirect_to users_path
+    @user = User.find(params[:following_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    @user = User.find(params[:user_id])
-    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
-    follow.destroy
-    redirect_to users_path
+    @user = User.find(params[:id])
+    current_user.unfollow(@user)
   end
 end
