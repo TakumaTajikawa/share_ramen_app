d3abd89a76379c0f5bb6473f5d515819da21b19d
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-20 15:40:38 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   get 'posts/:id/likes' => 'posts#likes'
+  resources :notifications, only: :index
   
 end
