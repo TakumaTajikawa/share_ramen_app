d05bda9ab35152cc71d46b207eec3b38d4f6e50f
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-25 18:50:02 +0900

    userモデルのテストを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :introduction, presence: false, length: { maximum: 200 } 
   mount_uploader :image, ImageUploader
