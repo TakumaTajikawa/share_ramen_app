dcfb15b9e39dc630848e9efe33b2ab8cd9061840
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-09-26 17:38:32 +0900

    userファクトリを作り、userモデルのテストを書き換え

--- config/application.rb
+++ config/application.rb
@@ -35,10 +35,10 @@ module ShareRamenApp
     config.generators.system_tests = nil
     config.generators do |g|
       g.test_framework :rspec,
-      fixtures: false,
       view_specs: false,
       helper_specs: false,
-      routing_specs: false
+      routing_specs: false,
+      request_specs: false
     end
   end
 end
