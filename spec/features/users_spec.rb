93939999b8e369f6dd80f4543a34d1c6f4581369
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-10-03 12:11:02 +0900

    usersのfeaturesテストがパスするよう変更

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Posts", type: :feature do
+RSpec.feature "Users", type: :feature do
 
   scenario "未登録ユーザーがアカウント登録できること" do
 
+    visit root_path
+    
     expect {
       click_link "新規登録"
       fill_in "ユーザー名", with: "山田太郎"
@@ -11,9 +13,6 @@ RSpec.feature "Posts", type: :feature do
       fill_in "パスワード", with: "yamada"
       fill_in "パスワード（確認用）", with: "yamada"
       click_button "アカウント登録"
-
-      expect(page).to have_content "アカウント登録しました"
-
-    }.to change(users_url, :count).by(1)
+    }.to change(User, :count).by(1)
   end
 end
