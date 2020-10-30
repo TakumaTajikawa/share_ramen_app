9b925ade19f59ea5d448b22afdc057ba2cc6b1ed
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-10-30 21:32:14 +0900

    本番環境の画像のアップロード先をAWS・S3に変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,7 +20,27 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   process :auto
-  storage :file
+
+  if Rails.env.development? || Rails.env.test? 
+    storage :file
+  else
+    storage :fog
+  end
+
+  # 画像ごとに保存するディレクトリを変える
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
+  end
+
+  # 許可する画像の拡張子
+  def extension_whitelist
+     %w(jpg jpeg gif png)
+  end
+
+  # ファイル名を書き換える
+  def filename
+    "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.#{file.extension}" if original_filename
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
